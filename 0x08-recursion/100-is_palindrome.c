3e76963735aaaef5ef86e787a11dd7af22815305
OkosodoNorah <ksdnorah@gmail.com>
2023-04-06 21:36:09 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,7 +2,6 @@
 
 int check_pal(char *s, int i, int len);
 int _strlen_recursion(char *s);
-
 /**
  * is_palindrome - checks if a string is a palindrome
  * @s: string to reverse
@@ -11,11 +10,10 @@ int _strlen_recursion(char *s);
  */
 int is_palindrome(char *s)
 {
-	if (*s === 0)
+	if (*s == 0)
 		return (1);
 	return (check_pal(s, 0, _strlen_recursion(s)));
 }
-
 /**
  * _strlen_recursion - returns the length of a string
  * @s: string to calculate the length of
@@ -24,7 +22,7 @@ int is_palindrome(char *s)
  */
 int _strlen_recursion(char *s)
 {
-	if (*s === '\0')
+	if (*s == '\0')
 		return (0);
 	return (1 + _strlen_recursion(s + 1));
 }
