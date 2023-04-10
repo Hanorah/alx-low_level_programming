db262fc2cf8627e7b6d005ede5fd9e4dd587a8ca
OkosodoNorah <ksdnorah@gmail.com>
2023-04-10 09:01:58 -0700

    4-add.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,30 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
 /**
-* main - multiplies two numbers.
-* @argc: argument count
-* @argv: arguments
-*
-* Return: 0
-*/
-int main(int argc, char **argv)
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
 {
-int e, f;
-
-
-if (argc < 3)
-{
-printf("Error\n");
-return (1);
-}
-
-
-e = atoi(argv[1]);
-f = atoi(argv[2]);
-printf("%d\n", e * f);
-
-
-return (0);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
