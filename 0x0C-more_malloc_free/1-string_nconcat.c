6b84fb66144e310fc127ca2420ff480152a76a6f
OkosodoNorah <ksdnorah@gmail.com>
2023-04-15 02:21:03 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,89 +1,126 @@
-#include <stdlib.h>
-		
 #include "main.h"
-		
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
 
-		
 /**
-		
- * *string_nconcat - concatenates n bytes of a string to another string
-		
- * @s1: string to append to
-		
- * @s2: string to concatenate from
-		
- * @n: number of bytes from s2 to concatenate to s1
-		
+ * _is_zero - determines if any number is zero
+ * @argv: argument vector.
  *
-		
- * Return: pointer to the resulting string
-		
+ * Return: no return.
  */
-		
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-		
+void _is_zero(char *argv[])
 {
-		
-	char *s;
-		
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
-		
+	int i, isn1 = 1, isn2 = 1;
 
-		
-	while (s1 && s1[len1])
-		
-		len1++;
-		
-	while (s2 && s2[len2])
-		
-		len2++;
-		
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
+		{
+			isn1 = 0;
+			break;
+		}
 
-		
-	if (n < len2)
-		
-		s = malloc(sizeof(char) * (len1 + n + 1));
-		
-	else
-		
-		s = malloc(sizeof(char) * (len1 + len2 + 1));
-		
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
+		{
+			isn2 = 0;
+			break;
+		}
 
-		
-	if (!s)
-		
-		return (NULL);
-		
-
-		
-	while (i < len1)
-		
+	if (isn1 == 1 || isn2 == 1)
 	{
-		
-		s[i] = s1[i];
-		
-		i++;
-		
+		printf("0\n");
+		exit(0);
 	}
-		
+}
 
-		
-	while (n < len2 && i < (len1 + n))
-		
-		s[i++] = s2[j++];
-		
+/**
+ * _initialize_array - set memery to zero in a new array
+ * @ar: char array.
+ * @lar: length of the char array.
+ *
+ * Return: pointer of a char array.
+ */
+char *_initialize_array(char *ar, int lar)
+{
+	int i = 0;
 
-		
-	while (n >= len2 && i < (len1 + len2))
-		
-		s[i++] = s2[j++];
-		
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
+	ar[lar] = '\0';
+	return (ar);
+}
 
-		
-	s[i] = '\0';
-		
+/**
+ * _checknum - determines length of the number
+ * and checks if number is in base 10.
+ * @argv: arguments vector.
+ * @n: row of the array.
+ *
+ * Return: length of the number.
+ */
+int _checknum(char *argv[], int n)
+{
+	int ln;
 
-		
-	return (s);
-		
+	for (ln = 0; argv[n][ln]; ln++)
+		if (!isdigit(argv[n][ln]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+
+	return (ln);
+}
+
+/**
+ * main - Entry point.
+ * program that multiplies two positive numbers.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ *
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
+	char *nout;
+
+	if (argc != 3)
+		printf("Error\n"), exit(98);
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	if (nout == NULL)
+		printf("Error\n"), exit(98);
+	nout = _initialize_array(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	for (; k >= 0; k--, i--)
+	{
+		if (i < 0)
+		{
+			if (addl > 0)
+			{
+				add = (nout[k] - '0') + addl;
+				if (add > 9)
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
+			}
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+		}
+		if (j < 0)
+		{
+			if (nout[0] != '0')
+				break;
+			lnout--;
+			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+		}
+		if (j >= 0)
+		{
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
+		}
+	}
+	printf("%s\n", nout);
+	return (0);
 }
